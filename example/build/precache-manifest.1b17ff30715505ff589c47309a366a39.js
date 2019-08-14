7e5b843468bcd7875aeee8cb1dcbcf7ee1a66dcf
Liam Hu <x10huwenhao@gmail.com>
2019-08-14 11:30:39 +0800

    Add the lastest CRA build example

---
+++ example/build/precache-manifest.1b17ff30715505ff589c47309a366a39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddb58dce9067ff370ee01fd3b7b39f14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5f0199faae6cd10b708",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "d3094c032bfbab601b29",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "b5f0199faae6cd10b708",
+    "url": "/static/js/2.f8fd391a.chunk.js"
+  },
+  {
+    "revision": "d3094c032bfbab601b29",
+    "url": "/static/js/main.63bf6d37.chunk.js"
+  },
+  {
+    "revision": "e1a6e0adc44a0ed3e01e",
+    "url": "/static/js/runtime~main.ea3e0b5a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
